2861d86960ec449fb7d75baa21aecb417f576cce
AitoolsPro <128456771+AitoolsPro@users.noreply.github.com>
2023-03-22 00:38:30 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -50,9 +50,9 @@ export default defineConfig({
       includeAssets: ["favicon.svg"],
       registerType: "autoUpdate",
       manifest: {
-        name: "ChatGPT",
+        name: "艾兔思Aitools-chatgpt",
         lang: "zh-cn",
-        short_name: "ChatGPT",
+        short_name: "艾兔思Aitools-chatgpt",
         background_color: "#f6f8fa",
         icons: [
           {

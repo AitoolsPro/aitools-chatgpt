6b6f50fab5abdb2002f0b48dfe35a0571ac2a494
AitoolsPro <128456771+AitoolsPro@users.noreply.github.com>
2023-03-27 14:02:01 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -15,7 +15,7 @@ export const defaultMessage = `欢迎使用 ChatGPT聊天机器人
 - 抖音/微信公众号/视频号：艾兔思Aitools
 -  [@艾兔思Aitools中文导航论坛](https://aitools.chat) 。
 - 设置：
-- 首次用需填网站暗号：aitools ，如自有APIKey请填写，现由我免费提供，故意滥用删ID。
+- 首次用需填网站暗号：aitools ，如自有API Key请填写，现由我免费提供，故意滥用删ID。
 - 系统角色指令：会在每次提问时添加，需加句号。主要对ChatGPT的语气，口头禅这些定制。（可不填）
 - 思维发散程度：越高 ChatGPT 思维就越发散，开始乱答。根据不同的问题可以调节这个选项，创意性的就可以调高一点。（默认中等）
 - 开启连续对话：OpenAI 并没提供ChatGPT那样的上下文功能，只能每次都把全部对话传过去，并且都要算 token，而且仍然有最大4096 token 的限制。
